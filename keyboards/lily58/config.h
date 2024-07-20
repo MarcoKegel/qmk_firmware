e5b0fc0d5455779f64b641300368e905e1f75c5a
Marco Meixner <info@meixners.org>
2024-07-20 20:41:35 +0200

    TappingTerm should part of the keymap and add layer descriptions

--- keyboards/lily58/config.h
+++ keyboards/lily58/config.h
@@ -24,6 +24,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_USB_DETECT
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 0
