2ea1d07763874cdc0be33d05ec72782bcbcaca79
Marco Meixner <info@meixners.org>
2025-03-20 08:28:40 +0100

    ferris: change modifier key to left also on the right sight on first layer

--- keyboards/ferris/keymaps/mini-ryoku/keymap.c
+++ keyboards/ferris/keymaps/mini-ryoku/keymap.c
@@ -29,7 +29,7 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// Colemak-dh
     [0] = LAYOUT_split_3x5_2(   KC_Q, KC_W, KC_F, KC_P, KC_B,                                               KC_J, KC_L, KC_U, KC_Y, KC_QUOT, 
-                                LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G,               KC_M, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), 
+                                LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G,               KC_M, LSFT_T(KC_N), LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O), 
                                 KC_Z, KC_X, KC_C, KC_D, KC_V,                                               KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, 
                                 LT(1,KC_TAB), LT(2,KC_SPC),                                                 LT(2,KC_ENT), LT(1,KC_BSPC)),
     // Navigation
