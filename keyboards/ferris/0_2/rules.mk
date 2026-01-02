b38d298761cfbc8bd1a1eadc0d1fb5de837c4fee
Marco Meixner <info@meixners.org>
2026-01-02 17:29:29 +0100

    cleanup

--- keyboards/ferris/0_2/rules.mk
+++ keyboards/ferris/0_2/rules.mk
@@ -2,8 +2,8 @@ CUSTOM_MATRIX = lite
 
 SRC += matrix.c
 I2C_DRIVER_REQUIRED = yes
-#DEFAULT_FOLDER = ferris/0_2/base
-#TAP_DANCE_ENABLE = yes
+
+COMBO_ENABLE = yes
 
 # this is caused by a bug - no idea why this is needed only in my code - see readme
 EXTRAINCDIRS += $(CHIBIOS)/os/oslib/include
